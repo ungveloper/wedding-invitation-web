76ae95443b5f8d7632a03c24d8085301e7820462
ungveloper <ungveloper@gmail.com>
2026-07-18 21:28:28 +0900

    추가: 폰트 나눔명조, 에스코어드림

--- app/lib/fonts.ts
+++ app/lib/fonts.ts
@@ -8,45 +8,53 @@ export const ChosunNm = localFont({
     },
   ],
 });
-/* 
-export const freesentation = localFont({
+
+export const NanumMyeongjo = localFont({
   src: [
     {
-      path: '../../../public/fonts/freesentation/900.woff2',
+      path: '../../public/fonts/NanumMyeongjo/400.woff2',
+      weight: '400',
+    },
+  ],
+});
+
+export const SCoreDream = localFont({
+  src: [
+    {
+      path: '../../public/fonts/SCoreDream/900.woff2',
       weight: '900',
     },
     {
-      path: '../../../public/fonts/freesentation/800.woff2',
+      path: '../../public/fonts/SCoreDream/800.woff2',
       weight: '800',
     },
     {
-      path: '../../../public/fonts/freesentation/700.woff2',
+      path: '../../public/fonts/SCoreDream/700.woff2',
       weight: '700',
     },
     {
-      path: '../../../public/fonts/freesentation/600.woff2',
+      path: '../../public/fonts/SCoreDream/600.woff2',
       weight: '600',
     },
     {
-      path: '../../../public/fonts/freesentation/500.woff2',
+      path: '../../public/fonts/SCoreDream/500.woff2',
       weight: '500',
     },
     {
-      path: '../../../public/fonts/freesentation/400.woff2',
+      path: '../../public/fonts/SCoreDream/400.woff2',
       weight: '400',
     },
     {
-      path: '../../../public/fonts/freesentation/300.woff2',
+      path: '../../public/fonts/SCoreDream/300.woff2',
       weight: '300',
     },
     {
-      path: '../../../public/fonts/freesentation/200.woff2',
+      path: '../../public/fonts/SCoreDream/200.woff2',
       weight: '200',
     },
     {
-      path: '../../../public/fonts/freesentation/100.woff2',
+      path: '../../public/fonts/SCoreDream/100.woff2',
       weight: '100',
     },
   ],
 });
- */
